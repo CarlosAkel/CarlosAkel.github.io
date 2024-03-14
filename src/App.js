fe90d329a96544f91323f2490ae6a3fa1d435ab9
CarlosAkel <cmakel@miuandes.cl>
2024-03-14 04:13:36 -0300

    new functionalities

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 
 import UrlShort from './components/UrlShort';
 import ShowUrl from './components/ShowUrl';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import LoggedPage from './components/LoggedPage';
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
@@ -10,6 +13,9 @@ function App() {
         <Routes>
           <Route path="/" element={<UrlShort />} />
           <Route path="/show" element={<ShowUrl />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/urls" element={<LoggedPage />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/UrlShort.js
+++ src/components/UrlShort.js
@@ -1,24 +1,55 @@
 
 import '../css/inputUrl.css'
 import '../css/button.css'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import GoogleIcon from '@mui/icons-material/Google';
+import Cookies from "js-cookie";
 function UrlShort() {
 
-  const MAINURL = "http://localhost:8000"
+  const MAINURL = "https://short-url-chile.onrender.com"
+  //const MAINURL = "http://localhost:8000"
   const [url, setUrl] = useState('');
   const navigate = useNavigate();
+
+
+  useEffect(() => {
+    //checkAuthenticationStatus();
+  }, []);
+
+  function checkAuthenticationStatus(popupWindow) {
+    const interval = setInterval(() => {
+      const bisc = Cookies.get("access_token");
+      if (bisc) {
+        popupWindow.close();
+        clearInterval(interval);
+        navigate('/urls');
+      }
+    }, 1000);
+  }
 
   async function url_guest() {
     const formData = new FormData();
     formData.append('url', url);
     try {
       const response = await axios.post(`${MAINURL}/url/guest`, formData);
-      localStorage.setItem("url",response.data.short_url)
+      localStorage.setItem("url", response.data.short_url)
       navigate('/show');
     } catch (error) {
       console.error('Error:', error);
+    }
+  }
+
+  function go_to_login() {
+    navigate('/login')
+  }
+  async function go_to_google_login() {
+    const go = MAINURL + "/google/login"
+    var popupWindow = window.open(go, "PopupWindow", "width=600,height=400");
+    if (popupWindow) {
+      popupWindow.focus();
+      checkAuthenticationStatus(popupWindow);
     }
   }
 
@@ -35,9 +66,16 @@ function UrlShort() {
       <div className="card">
         <div>Here you can login and obtain the list of your Urls</div>
         <div></div>
-        <button className='button_class'>
-          Login
-        </button>
+        <div className="login-container">
+          <button className='button_class' onClick={go_to_login}>
+            Login
+          </button>
+          <div>
+            or
+          </div>
+          <GoogleIcon sx={{ fontSize: 50 }} className='login-google-icon' onClick={go_to_google_login} />
+        </div>
+
       </div>
     </div>
   );
